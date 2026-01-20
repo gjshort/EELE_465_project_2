0a65e34396d4c81825d31bc42f123d2e59c79e7c
Griffin Short <gjshort@proton.me>
2026-01-20 11:22:13 -0700

    Configure LED1 as a heartbeat

--- src/main.asm
+++ src/main.asm
@@ -26,11 +26,34 @@ RESET       mov.w   #__STACK_END,SP
 
 
 init:
+
+            ; -- P1.0 (LED1 Heartbeat) --
+            bic.b #BIT0, &P1SEL0    ; Set to Digital I/O
+            bic.b #BIT0, &P1SEL1    ; "..."
+
+            bis.b #BIT0, &P1DIR     ; Set dir to out
+            bic.b #BIT0, &P1OUT     ; Clear output
+
+            ; -- TB0 (LED1 PWM) --
+            ; 0.5 second timer for 50% duty cycle and 1 sec. period
+            ; 0.5 sec = 1 us * 4 * 5 * 25,000
+            bic.w #TBCLR, &TB0CTL			; Clear timers/dividers
+            bis.w #TBSSEL__SMCLK, &TB0CTL	; 1 MHz ref clock
+            bis.w #MC__UP, &TB0CTL			; Up mode
+            bis.w #CNTL_0, &TB0CTL			; 16 bit counter
+
+            bis.w #ID__4, &TB0CTL			; Divide by 4
+            bis.w #TBIDEX__5, &TB0EX0		; Dicide by 5
+            bis.w #25000d, &TB0CCR0	   ; Set compare value
+
+            bic.w #CCIFG, &TB0CCTL0			; Clear interrupt flag
+	        bis.w #CCIE, &TB0CCTL0			; Enable interrupt
+
             ; Stop the watchdog timer
             mov.w   #WDTPW+WDTHOLD,&WDTCTL
-
             ; Disable low-power mode
             bic.w   #LOCKLPM5,&PM5CTL0
+            bis.w #GIE, SR              ; Enable global interrupts
 
 main:
 
@@ -40,10 +63,24 @@ main:
 
 
 
+;-------------------------------------------------------------------------------
+; Subroutines
+;-------------------------------------------------------------------------------
+
+; --- Timer B0 ISR ---
+TB0_CCR0_ISR:
+	xor.b #BIT0, &P1OUT		; Toggle LED1
+	bic.w #CCIFG, &TB0CCTL0	; Clear interrupt flag
+	reti
+
+
 ;------------------------------------------------------------------------------
 ;           Interrupt Vectors
 ;------------------------------------------------------------------------------
             .sect   RESET_VECTOR
             .short  RESET
+
+            .sect 	".int43"				; TB0CCR0 Vector
+	        .short	TB0_CCR0_ISR
 
             .end
